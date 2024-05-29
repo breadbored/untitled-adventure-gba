6076aa03c147cd4d0de1292aba7dead1d6bad418
breadcodes <brad@bread.codes>
2024-05-28 21:38:37 -0400

    Fix critical new game bug

--- src/save.cpp
+++ src/save.cpp
@@ -7,5 +7,9 @@ void save_game(SaveFiles save_files_to_save) {
 SaveFiles load_game() {
     SaveFiles save_files_to_load;
     bn::sram::read(save_files_to_load);
+    if (save_files_to_load.files[0].player_position_x == 0xFFFFFFFF && save_files_to_load.files[0].player_position_y == 0xFFFFFFFF && save_files_to_load.files[0].player_health == 0xFFFFFFFF && save_files_to_load.files[0].player_max_health == 0xFFFFFFFF) {
+        save_files_to_load = SaveFiles();
+        save_game(save_files_to_load);
+    }
     return save_files_to_load;
 }
