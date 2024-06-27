d7d932d00901bbba0d4af0abd5023107ed00385e
Jason Carpenter <5916026+breadbored@users.noreply.github.com>
2024-06-26 21:01:29 -0400

    Fix vectoring movement

--- src/utils.cpp
+++ src/utils.cpp
@@ -17,25 +17,35 @@ int random_int(int lower, int upper)
     return (rand_bc() % (upper - lower + 1)) + lower;
 }
 
-// I use this Quick Sqrt from Quake III and left in the comments for fun
-// https://en.wikipedia.org/wiki/Fast_inverse_square_root
-// enjoy the fuckery that is bit manipulation
-bn::fixed Q_rsqrt(bn::fixed numberFixed)
-{
-  long i;
-  float x2, y;
-  const float threehalfs = 1.5F;
-  float number = numberFixed.to_float();
+// // I use this Quick Sqrt from Quake III and left in the comments for fun
+// // https://en.wikipedia.org/wiki/Fast_inverse_square_root
+// // enjoy the fuckery that is bit manipulation
+// bn::fixed Q_rsqrt(bn::fixed numberFixed)
+// {
+//   long i;
+//   float x2, y;
+//   const float threehalfs = 1.5F;
+//   float number = numberFixed.to_float();
 
-  x2 = number * 0.5F;
-  y  = number;
-  i  = * ( long * ) &y;                       // evil floating point bit level hacking
-  i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
-  y  = * ( float * ) &i;
-  y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-  // y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+//   x2 = number * 0.5F;
+//   y  = number;
+//   i  = * ( long * ) &y;                       // evil floating point bit level hacking
+//   i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+//   y  = * ( float * ) &i;
+//   y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//   // y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
 
-  return y;
+//   return y;
+// }
+
+bn::fixed Q_rsqrt(bn::fixed numberFixed) {
+    float number = numberFixed.to_float();
+    if (number <= 0.0f) return 0.0f;
+    float approx = number * 0.5f;
+    for (int i = 0; i < 2; ++i) { // only 2 iterations for low precision
+        approx = (approx + number / approx) * 0.5f;
+    }
+    return approx;
 }
 
 bn::fixed lerpf( bn::fixed a, bn::fixed b, bn::fixed t )
